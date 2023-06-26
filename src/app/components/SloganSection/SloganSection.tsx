7147a6a89cce815cd502609789044090be7613f4
vitorLostadaC <vitorlostada@hotmail.com>
2023-06-26 17:47:27 -0300

    feat: responsive slogan

--- src/app/components/SloganSection/SloganSection.tsx
+++ src/app/components/SloganSection/SloganSection.tsx
@@ -4,20 +4,20 @@ import Image from "next/image"
 export const SloganSection = () => {
   return (
     <section className="flex h-[500px] flex-col items-center justify-center gap-4 bg-blue-100">
-      <p className="rounded-full rounded-bl-none border-2 border-solid border-primary-400 px-5 py-1 text-sm font-bold uppercase text-primary-400">
+      <p className="rounded-full rounded-bl-none border-2 border-solid border-primary-400 px-5 py-1 text-xs font-bold uppercase text-primary-400 md:text-sm">
         webinars Exclusivo
       </p>
-      <h2 className="text-5xl">Menos conversinha</h2>
-      <h1 className="relative bg-gradient-to-br from-primary-400 to-primary-500 bg-clip-text text-7xl font-bold text-transparent">
+      <h2 className="text-2xl sm:text-4xl md:text-5xl">Menos conversinha</h2>
+      <h1 className="relative bg-gradient-to-br from-primary-400 to-primary-500 bg-clip-text text-4xl font-bold text-transparent sm:text-6xl md:text-7xl">
         Mais Conversão
         <Image
           src={assetHeader}
           alt="decoração"
-          className="absolute right-0 top-0 translate-x-4"
+          className="absolute right-0 top-0 w-7 translate-x-3 sm:w-10 sm:translate-x-4"
         />
       </h1>
-      <hr className="h-[1px] w-full max-w-xl border-none bg-gray-300" />
-      <p>
+      <hr className="h-[1px] w-full max-w-[300px] border-none bg-gray-300 sm:max-w-lg md:max-w-xl" />
+      <p className="max-w-xs text-center text-xs sm:text-sm md:text-base ">
         Conheça as estratégias que{" "}
         <span className="font-bold">mudaram o jogo</span> e como aplicá-las no
         seu negócio
