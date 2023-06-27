3880a9865813dd8e5a309897a55222484c0de4e3
vitorLostadaC <vitorlostada@hotmail.com>
2023-06-26 22:48:29 -0300

    feat: responsive on Demonstration

--- src/app/components/Demonstration/Demonstration.tsx
+++ src/app/components/Demonstration/Demonstration.tsx
@@ -7,7 +7,7 @@ import montainDecoration from "@/assets/montainDecoration.svg"
 
 export const DemonstrationSection = () => {
   return (
-    <section className="relative  z-10 flex h-[600px] w-full items-center justify-center gap-10 bg-blue-100">
+    <section className="relative z-10 flex h-[600px] w-full items-center justify-center gap-10 bg-blue-100">
       <Image
         src={comparativeCTA}
         alt="Comparativo"
@@ -16,16 +16,16 @@ export const DemonstrationSection = () => {
 
       <div className="flex flex-col gap-3">
         <div>
-          <h3 className="max-w-[405px] text-4xl">
+          <h3 className="max-w-[405px] text-xl md:text-4xl">
             Pronto para triplicar sua{" "}
             <span className="font-bold">Geração de Leads ?</span>
           </h3>
-          <p className="mt-2">
+          <p className="mt-2 text-xs sm:text-base">
             Criação e ativação em <span className="font-bold">4 minutos</span>
           </p>
         </div>
 
-        <hr className="border-node h-[2px] bg-gray-400" />
+        <hr className="border-node h-[2px] bg-gray-400 " />
 
         <div className="flex items-center justify-center gap-4 xl:justify-start">
           <button className="rounded-full bg-primary-400 px-7 py-3 text-white">
@@ -45,7 +45,7 @@ export const DemonstrationSection = () => {
       <Image
         src={montainDecoration}
         alt="decoração"
-        className="absolute bottom-0 left-0 -z-10 -translate-x-1/3 "
+        className="absolute bottom-0 left-0 -z-10 hidden -translate-x-1/3 sm:block"
       />
       {/* <div className="abosulte h-80 w-80 rotate-45 bg-blue-200" /> */}
     </section>
